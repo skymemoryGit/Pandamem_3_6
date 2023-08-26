aac3acab47f00251c7a3551b1874043da9795d6b
skymemoryGit <49405036+skymemoryGit@users.noreply.github.com>
2023-08-26 21:27:28 +0000

    Update chatgpt.py

--- 3_6/Bot_telegram/chatgpt.py
+++ 3_6/Bot_telegram/chatgpt.py
@@ -6,7 +6,7 @@ Created on Mon Apr 10 19:21:39 2023
 """
 import openai
 
-openai.api_key ="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" #"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"
+openai.api_key ="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" #"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"
 
 
 def GetRispostaGpt(query):
